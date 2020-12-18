475164ee5dc036f42e90f400139472c668902b73
ryokosuge <ryo.kosuge@gmail.com>
2020-12-18 16:28:32 +0900

    feat:	Creating a CardView
    https://developer.apple.com/tutorials/app-dev-training/creating-a-cardview

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by ryokosuge on 2020/12/18.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    let title: String
+    let attendees: [String]
+    let lengthInMinutes: Int
+    let color: Color
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
+    }
+}
